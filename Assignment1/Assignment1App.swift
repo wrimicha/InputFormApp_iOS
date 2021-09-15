b2756cbde7d1d41ab59b90a908486ac22ed2441d
Michael Wright <wrimicha@sheridancollege.ca>
2021-09-15 15:48:58 -0400

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Michael Wright on 2021-09-15.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment1
+//
+//  Created by Michael Wright on 2021-09-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
